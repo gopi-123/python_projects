0b61c61f74123d1532a06ac3c2f5cdd3f5ddc61c
gmylapura <gmylapura@lsv.uni-saarland.de>
2018-11-17 22:07:30 +0100

    Hello World programming example & dealing with strings

---
+++ Hello_world.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 18 12:08:23 2018
+
+@author: gopinath
+"""
+# Programabout Dealing with String
+
+#Single quote
+print('Hello One')
+
+#Two quote string
+print("Hello Two !")
+
+
+
+#Three quote sting
+print('''Hello Three''')
+
+
+#Dealing with broken string -Enclosed wihtin single quote
+print('Ganesh Told that "Hello World I will see you soon!"')
