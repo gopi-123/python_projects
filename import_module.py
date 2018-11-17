2beb100bcb75256f70549d1f2f5967530135c219
gmylapura <gmylapura@lsv.uni-saarland.de>
2018-11-17 22:08:43 +0100

    importing modules math and its libraries

---
+++ import_module.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 13:24:43 2018
+
+@author: gopinath
+"""
+
+#importing a module math
+
+import math
+
+#dot operator calls functions of that module
+#ceil module
+print(math.ceil(3.8888))
+
+print(math.ceil(1.25))
+
+#floor module
+print(math.floor(3.8888))
+
+
+
+print(math.floor((1.25)))
+
+#power module  -- 2 power 3
+
+print(math.pow(2,3))
+
+print(math.pow(5,2))
+
+#print pi value 
+print(math.pi)
