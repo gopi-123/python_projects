2fca7da74289e14e1d29d8eb09f1fa06eee9222c
gmylapura <gmylapura@lsv.uni-saarland.de>
2018-11-17 22:03:54 +0100

    BODMAS Rule in Python for mathematical calculations

---
+++ BODMAS_rule.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 09:54:22 2018
+
+@author: gopinath
+"""
+
+
+#BODMAS Rule in Python for mathematical calculations
+#return what type of data it is storing
+
+a = 2
+
+print(type(2))
+
+
+
+a = 2.3
+
+print(type(2.3))
+
+a = "monalisa"
+
+print(type(a))
+
+
+
+a= 2*5-1
+print(a)
+
+
+#parenthesis have high precedence
+
+a=2*(5-1)
+print(a)
