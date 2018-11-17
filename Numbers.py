31fe40c1c80dc888ef18bbb107ad0389785d5bf6
gmylapura <gmylapura@lsv.uni-saarland.de>
2018-11-17 22:11:18 +0100

    Mathematical operations exercises

---
+++ Numbers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 10:05:38 2018
+
+@author: gopinath
+"""
+
+#Basic mathematical operations, modulo operation performed in numbers & variables
+#return what type of data it is storing
+
+#additon
+a = 2 + 3
+
+print(a)
+
+
+# subtraction
+a = 2 - 3
+print(a)
+
+#multiplication
+a=5 * 6
+print(a)
+
+#division
+a=6 / 2
+print(a)
+
+
+#modulo sign gives the remainder of division
+a= 10%2
+print(a)
+
+a= 11%2
+print(a)
+
+a= 11/2
+print(a)
